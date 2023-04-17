b59ffa0e6631e42ed075c05b86d529b4d3e29fc5
Antonio De Llamas <a2llamas@gmail.com>
2023-04-17 14:09:06 +0200

    Adicion elementos en main y archivo socios.dat

--- src/socioconjunto/Socio.java
+++ src/socioconjunto/Socio.java
@@ -1,8 +1,13 @@
 package socioconjunto;
 
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.IOException;
+import java.io.Serializable;
 import java.util.Date;
+import java.util.Scanner;
 
-public class Socio implements Comparable {
+public class Socio implements Serializable, Comparable {
     private String dni;
     private String nombre;
     private Date fechaAlta;
@@ -52,7 +57,7 @@ public class Socio implements Comparable {
 
     @Override
     public String toString() {
-        return this.getNombre() + " " + this.getDni() + " " + this.getFechaAlta();
+        return this.getNombre() + " - " + this.getDni() + " - " + this.getFechaAlta();
     }
 
     @Override
@@ -83,6 +88,60 @@ public class Socio implements Comparable {
 
     public static void main(String[] args) {
         
+        try{
+            ObjectInputStream input = new ObjectInputStream(new FileInputStream("extra/socios.dat"));
+            Scanner sc = new Scanner(System.in);
+            int opcion;
+            //TODO --> da error EOFException
+            do {
+                System.out.println("Menú de opciones:");
+                System.out.println("1. Alta");
+                System.out.println("2. Baja");
+                System.out.println("3. Modificación");
+                System.out.println("4. Listado por DNI");
+                System.out.println("5. Listado por antigüedad");
+                System.out.println("6. Salir");
+                System.out.print("Seleccione una opción: ");
+
+                opcion = sc.nextInt();
+                switch (opcion) {
+                    case 1:
+                        //Alta socio
+                        
+                        break;
+
+                    case 2:
+                        //Baja socio
+                        
+                        break;
+
+                    case 3:
+                        //Modificacion socio
+                        
+                        break;
+
+                    case 4:
+                        //Listado por DNI
+                        
+                        break;
+
+                    case 5:
+                        //Listado por antigüedad
+
+                        break;
+                    case 6:
+                        System.out.println("Saliendo del programa...");
+                        break;
+                    default:
+                        System.out.println("Opcion invalida, por favor seleccione una accion.");
+                }
+            } while(opcion != 6);
+
+        } catch (IOException e) {
+            System.out.println("Hubo un error al leer el archivo");
+            System.out.println(e);
+        }
+
     }
 
 }
